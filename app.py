d97a5b58fb94f564faeb2c2f18ed56cf5e1071a2
anandveeraswamy <anandveeraswamy@gmail.com>
2025-11-11 17:31:29 +0000

    Step2 - Folder structure introduced and moved routes to routes.py and imported it into the app.py. demonstrate circular imports

--- app.py
+++ app.py
@@ -2,19 +2,4 @@ from flask import Flask
 
 
 app = Flask(__name__)
-
-@app.route("/")
-def index():
-    return "<h1>Todo index page"
-
-@app.route("/tasks")
-def all_tasks():
-    return "<h1>List of all tasks</h1>"
-
-@app.route("/task/<int:task_id>")
-def task(task_id):
-    return f"<h1>Task detail page for task {task_id}</h1>"
-
-@app.route("/new-task")
-def create_task():
-    return f"<h1>Placeholder for creating a new task"
+import routes
