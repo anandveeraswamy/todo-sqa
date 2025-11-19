9d42cb92d8b1b9112cac2e822c9f2ac1ffe269fe
Anand <anandveeraswamy@gmail.com>
2025-11-19 22:21:09 +0000

    bdd: added failed login tests

--- tests/bdd/features/login.feature
+++ tests/bdd/features/login.feature
@@ -9,3 +9,18 @@ Feature: User Login
     And I am on the login page
     When I enter valid credentials
     Then I should be redirected to the homepage  
+
+  Scenario: Login fails with incorrect password
+    Given I am on the login page
+    When I enter an incorrect password
+    Then I should remain on the login page 
+
+  Scenario: Login fails with incorrect username
+    Given I am on the login page
+    When I enter an incorrect username
+    Then I should remain on the login page 
+
+  # Scenario: Logged-in user cannot see the login page again
+  #   Given I am already logged in
+  #   When I try to visit the login page
+  #   Then I should be redirected to the homepage
--- tests/bdd/steps/test_login_feature.py
+++ tests/bdd/steps/test_login_feature.py
@@ -23,10 +23,37 @@ def enter_valid_credentials(client):
         data= {"username": "testuser", "password": "Password123!"},
         follow_redirects = True
     )
-    return resp
-    
+    return resp    
 
 @then('I should be redirected to the homepage')
-def redirected_homepage(client, login_response):
+def redirected_homepage(login_response):
     assert login_response.status_code == 200
     assert b'Hello Testuser' in login_response.data
+    
+@given('I am on the login page')
+def on_login_page(client):
+    resp = client.get("/login")
+    assert resp.status_code == 200
+
+@when('I enter an incorrect password', target_fixture="login_response")
+def enter_incorrect_password(client):
+    resp = client.post(
+        '/login',
+        data= {"username": "testuser", "password": "wrong_password"},
+        follow_redirects = True
+    )
+    return resp
+
+@when('I enter an incorrect username', target_fixture="login_response")
+def enter_incorrect_username(client):
+    resp = client.post(
+        '/login',
+        data= {"username": "wrong_user", "password": "Password123!"},
+        follow_redirects = True
+    )
+    return resp
+
+@then('I should remain on the login page')
+def redirected_homepage(login_response):    
+    assert login_response.status_code == 200
+    assert b'Invalid username or password' in login_response.data
