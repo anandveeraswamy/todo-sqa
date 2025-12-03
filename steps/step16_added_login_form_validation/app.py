1077835bcaa1e1117db88b3bb4ba2d0888e95658
Anand <anandveeraswamy@gmail.com>
2025-12-03 16:03:24 +0000

    deleted the steps folder which crashed the tests

--- steps/step16_added_login_form_validation/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-from config import Config
-
-
-app = Flask(__name__)
-# loads all uppercase attributes from the Config class into the Flask application's configuration
-app.config.from_object(Config) 
-import routes
--- steps/step16_added_login_form_validation/config.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv # requires python-dotenv install
-load_dotenv()  # loads everything from .env into environment variables
-
-
-class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY') or 'replace-with-real-key-during-production'
